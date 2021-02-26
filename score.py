69bc6a3b700f680d84203a84c4caa469d3f4d5eb
Durgadas Gope <44217637+ddgope@users.noreply.github.com>
2021-02-26 21:00:42 +0000

    Add files via upload

--- score.py
+++ score.py
@@ -7,7 +7,7 @@ import pandas as pd
 def init():
     #This function initialises the model. The model file is retrieved used within the script.
     global model
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'best_hyperdrive_model.pkl') #name of model file (.sav or .pkl)
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'house_price_model.pkl') #name of model file (.sav or .pkl)
     print("Found model:", os.path.isfile(model_path)) #To check whether the model is actually present on the location we are looking at
     model = joblib.load(model_path)
 
